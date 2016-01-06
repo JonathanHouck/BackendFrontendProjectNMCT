f878ab29280cdf6e8798f554e6363aa34ec0700c
JonathanHouck <jona.houck@gmail.com>
2016-01-06 12:01:18 +0100

    alt + title on images title on links and inputs

--- Swoppr/public/js/controllers/CarouselCtrl.js
+++ Swoppr/public/js/controllers/CarouselCtrl.js
@@ -14,12 +14,14 @@
 
         slides.push({
             image: 'images/geldVerdienen.png',
-            text: 'Gebruik je enkel in het weekend uw grasmaaier? Verdien nu in de weekdagen geld ermee!'
+            text: 'Gebruik je enkel in het weekend uw grasmaaier? Verdien nu in de weekdagen geld ermee!',
+            title: 'Geld verdienen'
         });
 
         slides.push({
             image: 'images/kruiwagen.png',
-            text: 'Enkel bij het verhuizen een kruiwagen nodig? Huur nu een kruiwagen voor deze periode!'
+            text: 'Enkel bij het verhuizen een kruiwagen nodig? Huur nu een kruiwagen voor deze periode!',
+            title: 'Artikelen verhuren'
         });
     }
 }());
