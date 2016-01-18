5acc4427ebc219f5085f12f5504c6e8a63367c29
GregoireAmeye <gregoire.ameye@student.howest.be>
2016-01-18 12:44:30 +0100

    models tests

---
+++ Swoppr/tests/spec/controllers/DetailProductCtrl.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Greg on 18-Jan-16.
+// */
+//describe('MyCtrl', function () {
+//    var scope,
+//        controller;
+//
+//    /**
+//     * Created by Greg on 18-Jan-16.
+//     */
+//    beforeEach(inject(function($rootScope, $controller, dataSvc){
+//        scope=$rootScope.$new();
+//        mockDataSvc=dataSvc;
+//        spyOn(mockDataSvc,'save').andCallThrough();
+//        firstController = $controller('FirstController', {
+//            $scope: scope,
+//            dataSvc: mockDataSvc
+//        });
+//    }));
+//
+//
+//    it('should have assigned right pattern to numberPattern', function(){
+//        expect(scope.numberPattern).toBeDefined();
+//        expect(scope.numberPattern.test("100")).toBe(true);
+//        expect(scope.numberPattern.test("100aa")).toBe(false);
+//    })//
+
---
+++ Swoppr/tests/spec/services/ProductServiceSpec.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Greg on 18-Jan-16.
+ */
+//beforeEach(inject(function($rootScope, $controller, dataSvc){
+//    scope=$rootScope.$new();
+//    mockDataSvc=dataSvc;
+//    spyOn(mockDataSvc,'save').andCallThrough();
+//    firstController = $controller('FirstController', {
+//        $scope: scope,
+//        dataSvc: mockDataSvc
+//    });
+//}));
+//
+//
+//it('should have assigned right pattern to numberPattern', function(){
+//    expect(scope.numberPattern).toBeDefined();
+//    expect(scope.numberPattern.test("100")).toBe(true);
+//    expect(scope.numberPattern.test("100aa")).toBe(false);
+//});
